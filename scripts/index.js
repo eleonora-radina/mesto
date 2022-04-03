50d8faf6708de117ef5ba58f07af4a3a17ee4c3c
Eleonora Nikerina <eleonora.radina@mail.ru>
2022-04-03 05:41:07 +0400

    fix: исправлены общие ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,30 @@
 let editLink = document.querySelector('.profile__edit-button');
 let popupWindow = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
-
 let formElement = document.querySelector('.popup__form');
 let nameInput =  document.querySelector('.popup__form-item_el_name');
 let aboutInput = document.querySelector('.popup__form-item_el_about');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
-function togglePopUp() {
-  popupWindow.classList.toggle('popup_opened');
+function openPopUp() {
+  popupWindow.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   aboutInput.value = profileAbout.textContent;
 }
 
-function overlayClose(event) {
-  if (event.currentTarget === event.target) {
-    togglePopUp();
-  }
+function closePopUp() {
+  popupWindow.classList.remove('popup_opened');
 }
 
-editLink.addEventListener('click', togglePopUp);
-popupCloseButton.addEventListener('click', togglePopUp);
-popupWindow.addEventListener('click', overlayClose);
-
+// Обработчик «отправки» формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
-  togglePopUp();
+  closePopUp();
 }
+
+editLink.addEventListener('click', openPopUp);
+popupCloseButton.addEventListener('click', closePopUp);
 formElement.addEventListener('submit', formSubmitHandler);
